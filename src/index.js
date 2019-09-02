183a886faee0211f4cc5715d393e1dfa971c0307
Ashna Shah-Grover <as19@williams.edu>
2019-09-02 14:22:43 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
 });
+
+let form = document.querySelector('form')
+let list = document.querySelector('#list')
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  let input = form.querySelector("input").value
+  if (input) {
+    console.log(input)
+    list.innerHTML += `<li>${input}<button>X</button></li>`
+  }
+})
+
+list.addEventListener('click', event => {
+  if (event.target.tagName = "BUTTON"){
+    let remove = event.target.parentElement
+    remove.remove()
+  }
+})
