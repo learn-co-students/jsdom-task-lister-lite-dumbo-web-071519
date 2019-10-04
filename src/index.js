a7c937b4f8144386d691f6c124dc0f37f2c5f5e1
Aliciea Dawn Suechin <aliciea@Alicieas-MacBook-Pro.local>
2019-10-04 11:29:00 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskUl = document.getElementById("tasks");
+
+  let submitform = document.getElementById("create-task-form");
+  submitform.addEventListener("submit", function(event){
+    event.preventDefault();
+    let userInput = document.getElementById("new-task-description").value;
+    let li = document.createElement('li');
+
+    li.innerText = userInput;
+    taskUl.appendChild(li);
+    userInput = "";
+
+    // console.log(userInput);
+    taskUl.addEventListener("click", function(event){
+        if event.target.name === "button"{
+          taskUl
+
+        }
+
+    })
+
+  })
+
+  // taskTag.addEventListener("click", (e))
+});
 });
